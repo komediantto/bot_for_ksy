756c604ed1194413f463b8aae17da28fc073fca5
komediantto <komediantto@yandex.ru>
2022-09-03 00:00:32 +0200

    Фикс ошибки с классом

--- handlers/other.py
+++ handlers/other.py
@@ -20,7 +20,7 @@ class Measure:
         if mes_remains >= 0:
             await message.reply(f'Твой остаток - {mes_remains} ккал ')
         else:
-            await send_random_stickers(message)
+            send_random_stickers(message)
 
 
 # Принимаем на вход сообщение, извлекаем цифру калорий или жиров,
@@ -36,29 +36,11 @@ async def exp_cal(message: types.Message):
     id = message.from_user.id
     if 'калор' in message.text.lower():
         calories = Measure(cal_val)
-        calories.send_remains(message, 'max_calories', eaten=eaten, id=id)
-        # cal_remains = cal_val - eaten
-        # sqlite_db.cur.execute('UPDATE support '
-        #                       f'SET max_calories = {cal_remains} '
-        #                       f'WHERE id = {id}')
-        # sqlite_db.conn.commit()
-        # if cal_remains >= 0:
-        #     await message.reply(f'Твой остаток - {cal_remains} '
-        #                         f'ккал и {fat_val} грамм жиров')
-        # else:
-        #     await send_random_stickers(message)
+        await calories.send_remains(message, 'max_calories',
+                                    eaten=eaten, id=id)
     elif 'жир' in message.text.lower():
         fats = Measure(fat_val)
-        fats.send_remains(message, 'max_fats', eaten=eaten, id=id)
-        # fat_remains = fat_val - eaten
-        # sqlite_db.cur.execute('UPDATE support '
-        #                       f'SET max_fats = {fat_remains} WHERE id = {id}')
-        # sqlite_db.conn.commit()
-        # if fat_remains >= 0:
-        #     await message.reply(f'Твой остаток - {cal_val} '
-        #                         f'ккал и {fat_remains} грамм жиров')
-        # else:
-        #     await send_random_stickers(message)
+        await fats.send_remains(message, 'max_fats', eaten=eaten, id=id)
     else:
         pass
 
